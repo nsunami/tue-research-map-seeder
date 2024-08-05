e597e91cf7e596e15c5859e9e516a483df8050ff
Nami Sunami <nsunami@pm.me>
2024-08-05 16:12:09 +0200

    Update initial migration

--- prisma/migrations/20240805140420_init/migration.sql
+++ prisma/migrations/20240805140420_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Person" (
     "id" TEXT NOT NULL,
     "pureUuid" TEXT NOT NULL,
     "orcid" TEXT,
-    "firstName" TEXT NOT NULL,
+    "firstName" TEXT,
     "lastName" TEXT NOT NULL,
     "portalUrl" TEXT NOT NULL,
     "profilePhotoUrl" TEXT,
@@ -15,15 +15,16 @@ CREATE TABLE "Person" (
 CREATE TABLE "OrganizationMembership" (
     "id" TEXT NOT NULL,
     "pureId" BIGINT NOT NULL,
+    "email" TEXT,
     "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
-    "primaryAssociation" BOOLEAN NOT NULL,
-    "jobTitleUri" TEXT NOT NULL,
-    "jobTitle" TEXT NOT NULL,
-    "staffTypeUri" TEXT NOT NULL,
-    "staffType" TEXT NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "primaryAssociation" BOOLEAN,
+    "jobTitleUri" TEXT,
+    "jobTitle" TEXT,
+    "staffTypeUri" TEXT,
+    "staffType" TEXT,
     "personId" TEXT,
-    "organizationId" TEXT NOT NULL,
+    "organizationId" TEXT,
 
     CONSTRAINT "OrganizationMembership_pkey" PRIMARY KEY ("id")
 );
@@ -38,6 +39,18 @@ CREATE TABLE "Organization" (
     "type" TEXT NOT NULL,
 
     CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrganizationContact" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "pureId" BIGINT NOT NULL,
+    "type" TEXT NOT NULL,
+    "typeUri" TEXT NOT NULL,
+    "organizationId" TEXT,
+
+    CONSTRAINT "OrganizationContact_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -59,6 +72,9 @@ CREATE UNIQUE INDEX "OrganizationMembership_pureId_key" ON "OrganizationMembersh
 CREATE UNIQUE INDEX "Organization_pureUuid_key" ON "Organization"("pureUuid");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "OrganizationContact_pureId_key" ON "OrganizationContact"("pureId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_OrganizationRelation_AB_unique" ON "_OrganizationRelation"("A", "B");
 
 -- CreateIndex
@@ -68,7 +84,10 @@ CREATE INDEX "_OrganizationRelation_B_index" ON "_OrganizationRelation"("B");
 ALTER TABLE "OrganizationMembership" ADD CONSTRAINT "OrganizationMembership_personId_fkey" FOREIGN KEY ("personId") REFERENCES "Person"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OrganizationMembership" ADD CONSTRAINT "OrganizationMembership_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "OrganizationMembership" ADD CONSTRAINT "OrganizationMembership_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrganizationContact" ADD CONSTRAINT "OrganizationContact_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_OrganizationRelation" ADD CONSTRAINT "_OrganizationRelation_A_fkey" FOREIGN KEY ("A") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
